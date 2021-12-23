feaeaec11dee0e55ebbef4d55ae80dd27012fe44
provodoihi <profiloes@gmail.com>
2021-12-23 11:47:17 +0700

    Test scale function

--- src/DetailScreen/DetailScreen.tsx
+++ src/DetailScreen/DetailScreen.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {SafeAreaView, ScrollView, Text, View} from 'react-native';
+import {Image, SafeAreaView, ScrollView, Text, View} from 'react-native';
 import {AppNavigationProps} from '../navigation/routes';
 import {styles} from './style';
 
@@ -7,9 +7,25 @@ export const DetailScreen = ({navigation}: AppNavigationProps<'Detail'>) => {
   return (
     <SafeAreaView style={styles.container}>
       <ScrollView>
+        <Image
+          style={styles.mainProductImage}
+          source={require('../../assets/image/image15.png')}
+        />
+        <Image
+          style={styles.expandButton}
+          source={require('../../assets/icons/Group204.png')}
+        />
         <Text testID="2" style={styles.textDetailScreen}>
           Open Fashion App Detail
         </Text>
+        <View style={styles.productBox}>
+          <Text style={styles.productName}>M O H A N</Text>
+          <Text style={styles.productAttribute}>
+            Recycle Boucle Knit Cardigan Pink
+          </Text>
+          <Text style={styles.productPrice}>$ 120</Text>
+        </View>
+        <Text style={styles.textDetailScreen}>Open Fashion App Detail</Text>
       </ScrollView>
     </SafeAreaView>
   );
--- src/DetailScreen/style.ts
+++ src/DetailScreen/style.ts
@@ -1,4 +1,5 @@
 import {StyleSheet} from 'react-native';
+import {moderateScale} from '../utils';
 
 export const styles = StyleSheet.create({
   container: {
@@ -6,10 +7,58 @@ export const styles = StyleSheet.create({
     backgroundColor: '#ffffff',
   },
   textDetailScreen: {
-    fontSize: 18,
+    fontSize: moderateScale(18),
     fontWeight: '400',
     textAlign: 'center',
     fontFamily: 'TenorSans-Regular',
     color: '#000000',
+    marginTop: moderateScale(10),
+  },
+  mainProductImage: {
+    width: moderateScale(343),
+    height: moderateScale(460),
+    marginHorizontal: moderateScale(16),
+    marginTop: moderateScale(20),
+    marginBottom: moderateScale(13),
+    alignSelf: 'center',
+  },
+  expandButton: {
+    width: moderateScale(36),
+    height: moderateScale(36),
+    position: 'absolute',
+    top: moderateScale(432),
+    right: moderateScale(28),
+  },
+  productBox: {
+    width: moderateScale(343),
+    height: moderateScale(78),
+    marginTop: moderateScale(26),
+    marginBottom: moderateScale(18),
+    marginVertical: moderateScale(16),
+    alignSelf: 'center',
+    alignItems: 'flex-start',
+  },
+  productName: {
+    fontSize: moderateScale(16),
+    fontWeight: '400',
+    textAlign: 'left',
+    fontFamily: 'TenorSans-Regular',
+    color: '#000000',
+  },
+  productAttribute: {
+    fontSize: moderateScale(16),
+    fontWeight: '400',
+    textAlign: 'left',
+    fontFamily: 'TenorSans-Regular',
+    color: '#555555',
+    paddingTop: moderateScale(8),
+  },
+  productPrice: {
+    fontSize: moderateScale(18),
+    fontWeight: '400',
+    textAlign: 'left',
+    fontFamily: 'TenorSans-Regular',
+    color: '#DD8560',
+    paddingTop: moderateScale(8),
   },
 });
